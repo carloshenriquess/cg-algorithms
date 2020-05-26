0de447992e744f9376bc8730d7cb41f7228e5cf5
Carlos Henrique <carlos.henrique0307200@gmail.com>
2020-05-25 22:34:48 -0300

    estrutura do canvas DDA

--- src/scripts/dda.js
+++ src/scripts/dda.js
@@ -1,5 +1,13 @@
 export const dda = () => {
-  const form = document.getElementById('dda-form');
+  const canvas = document.getElementById('dda-canvas');
+  const context = canvas.getContext('2d');
+  const fillBgCanvas = color => {
+    context.beginPath();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = color;
+    context.fill();
+  };
+
   const onSubmit = () => {
     const x1 = document.getElementById('dda-x1').value;
     const y1 = document.getElementById('dda-y1').value;
@@ -8,6 +16,9 @@ export const dda = () => {
     console.log(`(${x1},${y1})`);
     console.log(`(${x2},${y2})`);
   };
+
+  fillBgCanvas('#c6c3ff');
+
+  const form = document.getElementById('dda-form');
   form.addEventListener('submit', onSubmit);
-  dda.hide = () => {};
 };
